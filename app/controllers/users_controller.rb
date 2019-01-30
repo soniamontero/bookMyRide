097442bc80b0a1162fc40a0ff5c8582c13c43ff2
Sonia Montero <sonia.montero@skema.edu>
2019-01-30 12:18:17 +0800

    updated user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :location, :owner, :email, :avatar)
+    params.require(:user).permit(:name, :email, :avatar)
   end
 end
