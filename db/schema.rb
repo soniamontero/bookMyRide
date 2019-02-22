bd9c59e7fff9a3464aef2341d702ee491f66a250
Sonia Montero <sonia.montero@skema.edu>
2019-02-22 17:46:48 +0800

    nvabar change, added reviews to booking / ride have reviews through bookings

--- db/schema.rb
+++ db/schema.rb
@@ -68,13 +68,11 @@ ActiveRecord::Schema.define(version: 2019_02_22_042423) do
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.string "message"
+    t.bigint "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rating"
-    t.bigint "ride_id"
-    t.bigint "user_id"
-    t.index ["ride_id"], name: "index_reviews_on_ride_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "rides", force: :cascade do |t|
@@ -112,7 +110,6 @@ ActiveRecord::Schema.define(version: 2019_02_22_042423) do
 
   add_foreign_key "bookings", "rides"
   add_foreign_key "bookings", "users"
-  add_foreign_key "reviews", "rides"
-  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "bookings"
   add_foreign_key "rides", "users"
 end
