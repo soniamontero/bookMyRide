b9a66cb6ca5c8b8ec818249ec96c70cb1803cf32
Sonia Montero <sonia.montero@skema.edu>
2019-05-27 18:45:28 +0800

    updated facebook credentials for omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,9 +257,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
-  facebook_id = Rails.application.credentials.dig(:facebook, :access_key_id)
-  facebook_secret = Rails.application.credentials.dig(:facebook, :secret_access_key)
+  facebook_id = Rails.application.credentials.dig(Rails.env.to_sym, :facebook, :access_key_id)
+  facebook_secret = Rails.application.credentials.dig(Rails.env.to_sym, :facebook, :secret_access_key)
 
   config.omniauth :facebook, facebook_id, facebook_secret, scope: 'email'
   # ==> Warden configuration
