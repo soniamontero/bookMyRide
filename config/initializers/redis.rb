b3b76be21731c17d5ede67be7b8291b09fad2d87
Sonia Montero <sonia.montero@skema.edu>
2019-05-29 14:23:19 +0800

    FIXED FOLDERSSSSS

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-REDIS = Redis.new
+$redis = Redis.new
 
 url = Rails.application.credentials.dig(Rails.env.to_sym, :redis, :url)
 
@@ -10,5 +10,5 @@ if url
   Sidekiq.configure_client do |config|
     config.redis = { url: url }
   end
-  REDIS = Redis.new(url: url)
+  $redis = Redis.new(url: url)
 end
